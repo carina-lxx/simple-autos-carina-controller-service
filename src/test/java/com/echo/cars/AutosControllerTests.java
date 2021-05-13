7b3e0a1dda7629748a70869da0f5fe46889c2bad
codingfox <carinalxx@gmail.com>
2021-05-12 21:23:22 -0700

    Test getAutos_searchParms_exists_returnsAutosList passes and some refactoring

--- src/test/java/com/echo/cars/AutosControllerTests.java
+++ src/test/java/com/echo/cars/AutosControllerTests.java
@@ -5,15 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,9 +42,36 @@ public class AutosControllerTests {
                 .andExpect(jsonPath("$.automobiles", hasSize(5)));
     }
         // GET:  /api/autos   returns 204 no autos found
+
+    @Test
+    void getAutos_noParms_non_returnsNoContent() throws Exception {
+        // Arrange
+        when(autosService.getAutos()).thenReturn(new AutosList());
+        // Act
+        mockMvc.perform(get("/api/autos"))
+                .andDo(print())
+        // Assert
+                .andExpect(status().isNoContent());
+    }
+
         // GET:  /api/autos?color=RED   returns all red cars
         // GET:  /api/autos?make=Ford   returns all Ford
         // GET:  /api/autos?make=Ford&color=GREEN   returns green ford
+
+    @Test
+    void getAutos_searchParms_exists_returnsAutosList() throws Exception {
+        // Arrange
+        List<Automobile> automobiles = new ArrayList<>();
+        for(int i = 0; i < 5; i++) {
+            automobiles.add(new Automobile(1967+i, "Ford", "Mustang", "AABB"+i));
+        }
+        when(autosService.getAutos(anyString(), anyString())).thenReturn(new AutosList(automobiles));
+        // Act
+        mockMvc.perform(get("/api/autos?color=RED&make=Ford"))
+        // Assert
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.automobiles", hasSize(5)));
+    }
 
 
     // POST: /api/autos
